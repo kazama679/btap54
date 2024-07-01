cb1fcca3f3b21692a0fcf0430f716b2e8fba8623
kazama679 <quanglienha123@gmail.com>
2024-07-01 09:02:43 +0700

    a

---
+++ client/src/store/store.ts
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "./reducers/productReducer";
+
+const store=configureStore({
+    reducer:{
+        productReducer,
+    }
+})
+export default store;
